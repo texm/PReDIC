978027a87a911f2da472aad0f5b1a6d4707e6fe5
Tex <texmoran2@gmail.com>
2019-08-29 21:41:04 +0800

    Refactor package code & create stubs for testing

---
+++ deformation_measurement/C_First_Order.py
@@ -0,0 +1,6 @@
+def C_First_Order(q):
+	C = 0.0
+	GRAD = 0.0
+	HESS = 0.0
+
+	return C, GRAD, HESS
---
+++ deformation_measurement/DIC_NR_images.py
@@ -0,0 +1,4 @@
+def DIC_NR_images(ref_img, def_img, subsetSize, ini_guess):
+	result = []
+
+	return result
