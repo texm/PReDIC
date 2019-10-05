5c7996a01861fedd01145095080188d14b9e7bdc
JamesCaldon <james.caldon@outlook.com>
2019-10-05 23:42:08 +0800

    Update Manual Translation

--- PYTHON/DIC_NR_images_manual.py
+++ PYTHON/DIC_NR_images_manual.py
@@ -1,6 +1,10 @@
 #import C_First_Order
-import matplotlib.image
-import numpy
+from PIL import Image
+import numpy as np
+import math
+from scipy.interpolate import BPoly
+from scipy.interpolate import NdPPoly
+import matplotlib.pyplot as plt
 global subset_size
 global ref_image
 global Xp
@@ -15,16 +19,16 @@ def DIC_NR_images(ref_img=None,def_img=None,subsetSize=None,ini_guess=None,*args
     if (subsetSize % 2 == 0):
         raise ValueError("Subset size must be odd")
 
-    # Prepare for trouble (load images) (default directory is current working directory)
-    ref_image = matplotlib.image.imread(ref_img) # numpy.array
-    def_image = matplotlib.image.imread(def_img) # numpy.array
+    # Prepare for trouble (load images) (default directory is current working directory) https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
+    ref_image = np.array(Image.open(ref_img).convert('LA')) # numpy.array
+    def_image = np.array(Image.open(def_img).convert('LA')) # numpy.array
 
     # Make it double
     ref_image = ref_image.astype('d') # convert to double
     def_image = def_image.astype('d') # convert to double
-
+    print(def_image.shape)
     # Obtain the size of the reference image
-    X_size, Y_size = ref_image.shape
+    X_size, Y_size, _tmp= ref_image.shape
 
     # Initialize variables
     subset_size = subsetSize
@@ -48,21 +52,104 @@ def DIC_NR_images(ref_img=None,def_img=None,subsetSize=None,ini_guess=None,*args
     Ymax = round(Y_size-((subset_size/2) +15))
     Xp = Xmin
     Yp = Ymin
-
+    print(type(Xp))
     if ( (Xp < Xmin) or (Yp < Ymin) or (Xp > Xmax) or  (Yp > Ymax) ):
         raise ValueError('Process terminated!!! First point of centre of subset is on the edge of the image. ');
     
     #_____________Automatic Initial Guess_____________
 
     # Automatic Initial Guess
-    q_0 = numpy.zeros_like([], shape=(6,1))
-    q_0 = numpy.zeros_like([], shape=(6)).T
-    print(q_0)
-    q_0[0:2,0] = ini_guess
+    q_0 = np.zeros_like([], shape=(6))
+    q_0[0:2] = ini_guess
     range_ = 15 # Minus 1 for array starting at zero?
-    print(q_0[0][0])
-    u_check = numpy.arange((round(q_0[0]) - range_), (round(q_0[1]) + range_))
-    print(u_check)
+    u_check = np.arange((round(q_0[0]) - range_), (round(q_0[1]) + range_)+1, dtype=int)
+    v_check = np.arange((round(q_0[0]) - range_), (round(q_0[1]) + range_)+1, dtype=int)
+
+    # Define the intensities of the first reference subset
+    subref = ref_image[Yp-math.floor(subset_size/2):(Yp+math.floor(subset_size/2))+1, Xp-math.floor(subset_size/2):Xp+math.floor(subset_size/2)+1,0]
+    print(subref)
+    
+    # Preallocate some matrix space
+    sum_diff_sq = np.zeros((u_check.size, v_check.size))
+    # Check every value of u and v and see where the best match occurs
+    for iter1 in range(u_check.size):
+        for iter2 in range(v_check.size):
+            subdef = def_image[(Yp-math.floor(subset_size/2)+v_check[iter2]):(Yp+math.floor(subset_size/2)+v_check[iter2])+1, (Xp-math.floor(subset_size/2)+u_check[iter1]):(Xp+math.floor(subset_size/2)+u_check[iter1])+1,0]
+
+            sum_diff_sq[iter2,iter1] = sum(sum(np.square(subref-subdef)))
+    print(subdef)
+    OFFSET1 = np.argmin(np.min(sum_diff_sq, axis=1)) # These offsets are +1 in MATLAB
+    OFFSET2 = np.argmin(np.min(sum_diff_sq, axis=0))
+    print(OFFSET1)
+    print(OFFSET2)
+    q_0[0] = u_check[OFFSET1]
+    q_0[1] = u_check[OFFSET2]
+    del u_check
+    del v_check
+    del iter1 
+    del iter2 
+    del subref 
+    del subdef
+    del sum_diff_sq
+    del OFFSET1 
+    del OFFSET2
+
+
+    # Preallocate the matrix that holds the deformation parameter results
+    DEFORMATION_PARAMETERS = np.zeros_like([], shape=(Y_size,X_size,12))
+
+    # Set the initial guess to be the "last iteration's" solution.
+    q_k = q_0[1:6]
+
+    #_______________COMPUTATIONS________________
+
+    # Start the timer: Track the time it takes to perform the heaviest computations
+    #tic????
+
+    #__________FIT SPLINE ONTO DEFORMED SUBSET________________________
+    # Obtain the size of the reference image
+    Y_size, X_size,tmp = ref_image.shape
+    
+    # Define the deformed image's coordinates
+    X_defcoord = np.arange(0,X_size, dtype=int) # Maybe zero?
+    Y_defcoord = np.arange(0,Y_size, dtype=int)
+
+    #print(np.array([X_defcoord, Y_defcoord]).shape)
+    # Fit the interpolating spline: g(x,y)
+    #def_interp = spapi( [spline_order, spline_order], [Y_defcoord, X_defcoord], def_image[Y_defcoord,X_defcoord] )
+
+
+
+    #test = interpolate.splrep()
+
+    spl = NdPPoly(def_image[:,:,0], (Y_defcoord, X_defcoord) )
+
+    fig, ax = plt.subplots()
+    xx = np.linspace(1.5, 4.5, 50)
+    ax.plot(xx, spl(xx), 'b-', lw=4, alpha=0.7, label='BSpline')
+    ax.grid(True)
+    ax.legend(loc='best')
+    plt.show()
+
+    exit()
+    def_interp = BPoly.from_derivatives(def_image[:,:,0],  [[X_defcoord], [Y_defcoord]], orders=[spline_order, spline_order])
+    #print(def_interp)
+    # Find the partial derivitives of the spline: dg/dx and dg/dy
+    #def_interp_x = fnder(def_interp, [0,1])
+    #def_interp_y = fnder(def_interp, [1,0])
+
+    # Convert all the splines from B-form into ppform to make it
+    # computationally cheaper to evaluate. Also find partial derivatives of
+    # spline w.r.t x and y
+    #def_interp = fn2fm(def_interp, 'pp')
+    #def_interp_x = fnder(def_interp, [0,1])
+    #def_interp_y = fnder(def_interp, [1,0])
+    #_________________________________________________________________________ 
+    t_interp = toc;    # Save the amount of time it took to interpolate
     return
 
-DIC_NR_images("ref500.bmp", "def500.bmp", 7, [4, 2])
+    
+
+
+
+DIC_NR_images("ref50.bmp", "def50.bmp", 7, [0, 0])
