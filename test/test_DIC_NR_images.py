bdce5cf167e20863253e175da1a49586b4c60f5d
Tanya Stolarski <tanya.stolarski@gmail.com>
2019-10-21 15:26:00 +0800

    test clean up

--- test/test_DIC_NR_images.py
+++ test/test_DIC_NR_images.py
@@ -117,91 +117,22 @@ class Test_DIC_NR(unittest.TestCase):
 	def test_newton_raphson(self):
 		#TODO:
 		print()
-
-	def test_something(self):
-		'''
-		test_image_1 = np.array(Image.open(TEST_IMAGE_DIR + "ref50.bmp").convert('LA')) # numpy.array
-		test_image_1 = test_image_1.astype('d')
-
-		ref_img = np.expand_dims(np.reshape(np.arange(40*40), (40,40)), axis = 2)
-		ref_img = np.insert(ref_img, 1, 0, axis = 2)
-		def_img = np.roll(ref_img.copy(), 1, axis = 1)
-
-		print("ref image:")
-		print(ref_img[15:26,15:26,0])
-
-		print("def image:")
-		print(def_img[15:26,16:27,0])
-
-		dic = DIC_NR(ref_img, def_img, 11, [0,0])
-		dic.initial_guess()
-		dic.fit_spline()
-
-		print(dic.q_k)
-		print(dic.Xp)
-		print(dic.Yp)
-
-		output = dic.calculate()
-
-		#c_last, grad_last, hess = dic.cfo.calculate(dic.q_k, dic.Xp, dic.Yp)
-		'''
-		#print(output[20,20,0])
-		#print(output[20,20,1])
-		'''
-		print("C")
-		print(c_last)
-		print("grad")
-		print(grad_last)
-		print("hess")
-		print(hess)
-		'''
-
 		
+
+	def test_whole(self):
 		dic_2 = DIC_NR()
 		dic_2.set_parameters(TEST_IMAGE_DIR + "ref50.bmp", TEST_IMAGE_DIR + "def50.bmp",11,[0,0])
-		dic_2.initial_guess()
-		dic_2.fit_spline()
-
-		#c_last_2, grad_last_2, hess_2 = dic_2.cfo.calculate(dic_2.q_k, dic_2.Xp, dic_2.Yp)
 		
 		output_2 = dic_2.calculate()
-		#print("other")
-		print(output_2[20:30,20:30,0])
-		print(output_2[20:30,20:30,1])
+
+		self.assertEqual(output_2[20,20,0], 2)
+		self.assertTrue(math.isclose(output_2[20,20,1], 0.0, abs_tol = 0.01))
 
 		x,y,z = output_2.shape
 		sav = np.swapaxes(output_2, 2, 1).reshape((x, y*z), order='A')
 
 		savetxt_compact("output",sav)
-		
 
-	def test_whole(self):
-		dic = DIC_NR()
-		dic.set_parameters(TEST_IMAGE_DIR + "ref50.bmp", TEST_IMAGE_DIR + "def50.bmp", 7, [0, 0])
-		print("Not running whole calculation")
-		'''result = dic.calculate()
-		x,y,z = result.shape
-		sav = np.swapaxes(result, 2, 1).reshape((x, y*z), order='A')
-
-		savetxt_compact("output", sav)'''
-
-	''' no file def_500_19?
-	def test_gen(self):
-		#test_image_1 = np.array(Image.open(TEST_IMAGE_DIR + "def_500_19.bmp").convert('LA')) # numpy.array#
-		#test_image_1 = test_image_1.astype('d')
-		#print(test_image_1)
-
-		dic = DIC_NR()
-		dic.set_parameters(TEST_IMAGE_DIR + "ref_500_19.bmp", TEST_IMAGE_DIR + "def_500_19.bmp", 11, [0,0])
-		dic.initial_guess()
-		dic.fit_spline()
-		output = dic.calculate()
-
-		x,y,z = output.shape
-		sav = np.swapaxes(output_2, 2, 1).reshape((x, y*z), order='A')
-
-		#savetxt_compact("output",sav)
-	'''
 
 def savetxt_compact(fname, x, fmt="%.6g", delimiter=','):
 	with open(f"compact_{fname}.csv", 'w+') as fh:
